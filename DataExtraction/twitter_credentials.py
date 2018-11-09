9627be5d672e8660c8be045cf39ac007973033c5
Lavkush Singh <31582871+lavkushsingh31@users.noreply.github.com>
2018-11-09 19:25:00 +0530

    Create twitter_credentials.py

---
+++ DataExtraction/twitter_credentials.py
@@ -0,0 +1,10 @@
+# Steps to get Twitter API credentials :
+# a) Create Twitter Developer Account
+# b) Create an app by signing in to the developer account
+# c) Generate the Access token keys and copy the keys from there
+
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "1046284094118625280-el4Tzo2cL9n5n0LdkaU7ltJsthwg4f"
+ACCESS_TOKEN_SECRET = "mz1xspQS9BSVtUgJQhnwN9k52L4mwWlyIMkP1ezHFl4Wc"
+CONSUMER_KEY = "wWt1l2oSLyqkfcHnhCzrJG0FH"
+CONSUMER_SECRET = "cWUCAJfh2uUYA5nIreLUwF0QkTkweJtonUnYGU51LSjPH50kU0"
