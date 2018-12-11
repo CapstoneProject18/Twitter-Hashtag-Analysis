5c6a01493461ca008c0956c3fbfffba136479f8d
Rishabh Kumar Kandoi <rishabhk8@gmail.com>
2018-12-11 10:56:15 +0530

    Update graphOutput.py.

--- DataVisualization/graphOutput.py
+++ DataVisualization/graphOutput.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from flaskblog.FileLocations import file_locations
 
 
 class GenerateGraph:
@@ -8,13 +9,18 @@ class GenerateGraph:
         pass
 
     def num_hashtags(self, d, plt):
+        fig = plt.figure()
         plt.bar(d.count()['tweets'].index.values, d.count()['tweets'].values)
         plt.xlabel("Date")
         plt.ylabel("Number of Hashtags")
         plt.title("Datewise Hashtag usages")
-        plt.show()
+        plt.xticks(rotation=90)
+        # plt.show()
+        fig.savefig(file_locations.numofhashtags)
+
 
     def num_likes_retweets(self, d, plt):
+        fig = plt.figure()
         likes = d['likes'].sum()
         retweet = d['retweet'].sum()
         plt.plot(likes.index.values, likes.values, color='g', label="Likes")
@@ -23,9 +29,12 @@ class GenerateGraph:
         plt.ylabel("Number of likes/retweets")
         plt.title("Datewise Hashtag usages")
         plt.legend(shadow=True, fontsize='x-large')
-        plt.show()
+        plt.xticks(rotation=90)
+        # plt.show()
+        fig.savefig(file_locations.numoflikes)
 
     def top_hashtags(self, df, plt):
+        fig = plt.figure()
         grouped = df.groupby('username').count().reset_index()
         people = (grouped.sort_values('tweets', ascending=False)).head(10)
         usernames = people['username'].str.extract(r'\'(.*)\'').values
@@ -36,14 +45,15 @@ class GenerateGraph:
         plt.ylabel("Number of Hashtags")
         plt.title("Hashtag Usages by top 10 users")
         plt.xticks(rotation=90)
-        plt.show()
+        # plt.show()
+        fig.savefig(file_locations.toptenusers)
 
 
 if __name__ == '__main__':
 
     graph = GenerateGraph()
-    df = pd.read_csv("C:/Users/hp/Desktop/Capstone2018/data.csv",
-                     names=['created_at', 'tweets', 'username', 'likes', 'retweet'])
+    df = pd.read_csv(file_locations.dataset,
+                     names=['created_at', 'tweets', 'username', 'likes', 'retweet', 'coordinates'])
     d = df.groupby(df["created_at"].str[0:10])
     graph.num_hashtags(d, plt)
     graph.num_likes_retweets(d, plt)
